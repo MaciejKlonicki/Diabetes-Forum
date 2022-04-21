e7ea4f598af01d44d78bb88ad88eab1142b40bdc
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-04-21 14:28:45 +0200

    I deleted the h1

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 function Body() {
   return (
     <div className='body-container'>
-      <h1>Forum internetowe dla osób z cukrzycą</h1>
         <BackImage />
     </div>
   )

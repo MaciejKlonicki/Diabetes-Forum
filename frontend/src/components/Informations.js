5c3e43a790106b459d2591c7024bf99abd556192
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-05-11 13:26:32 +0200

    Now im adding a images to my info page - and i will try to make css animation to show text when hover the image

--- frontend/src/components/Informations.js
+++ frontend/src/components/Informations.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Body.css';
+import { Link } from 'react-router-dom';
+import './Informations.css'
 
 function Informations() {
   return (
-
     <div>
         <BackImage />
-        <Cards />
+        <ImagesForInformations
+          img='/images/informations1.jpg'/>
     </div>
   )
 }
@@ -19,12 +21,18 @@ function BackImage() {
     )
   }
 
-  function Cards () {
-      return (
-          <div>
-              asd
-          </div>
-      );
+  function ImagesForInformations(props) {
+    return (
+      <div className='image-for-info'>
+        <Link to={props.path}>
+          <img
+          src={props.img}
+          alt='Informations'
+          className='info-photo'
+          />
+        </Link>
+      </div>
+    );
   }
 
 export default Informations

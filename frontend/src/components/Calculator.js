197652477545445c75719484975e2fc67df83dfc
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-06-28 11:03:44 +0200

    I added a first breakfast field and second breakfast field now im trying to add them togheter

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import './Calculator.css'
+import React from 'react';
+import './Calculator.css';
+import {useState} from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 function Calculator() {
   return (
@@ -33,9 +35,25 @@ function BackImage() {
   }
 
   var correctNumber;
-  // var firstBreakfast = "Pierwsze śniadanie";
+  var lunch;
+  var tea;
+  var firstDinner;
+  var secondDinner;
 
   function Correct() {
+    const [firstBreakfast, setFirstBreakfast] = useState('');
+    const [secondBreakfast, setSecondBreakfast] = useState('');
+
+    const handleChange = event => {
+      setFirstBreakfast(event.target.value);
+    };
+
+    const handleChange2 = event => {
+      setSecondBreakfast(event.target.value);
+    };
+
+    const wynik =  firstBreakfast + secondBreakfast;
+
     return (
       <div>
         <p className="calculator-desc-2">
@@ -49,6 +67,25 @@ function BackImage() {
           </p>
           <p className='calculator-desc-3'>
             Posiłki
+            <input
+            className='first-breakfast'
+            type={propTypes.number}
+            id='firstBreakfast'
+            onChange={handleChange}
+            name='firstBreakfast'
+            value={firstBreakfast}> 
+            </input><br /><br />
+            <p className='first-breakfast-position'>pierwsze śniadanie</p>
+            <input
+            className='second-breakfast'
+            type={propTypes.number}
+            id='secondBreakfast'
+            onChange={handleChange2}
+            name='secondBreakfast'
+            value={secondBreakfast}> 
+            </input><br />
+            <p className='second-breakfast-position'>drugie śniadanie</p>
+            <p> {"Wynik = " + wynik}</p>
           </p>
       </div>
     )

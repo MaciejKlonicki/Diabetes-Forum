4f95789f2c24a943a9b22caec1ffa186e8b09f24
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-08-12 17:37:42 +0200

    I deleted the whole structure of my backend to make it work different

--- backend/src/main/java/com/maciejklonicki/org/backend/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.maciejklonicki.org.backend.service;
-
-import com.maciejklonicki.org.backend.model.Users;
-import com.maciejklonicki.org.backend.web.dto.UserRegistrationDto;
-
-public interface UserService {
-     Users save(UserRegistrationDto registrationDto);
-}

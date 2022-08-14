893cb64cfecb481d0e3c56f7a3e51617738a510f
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-08-14 10:33:05 +0200

    I finished UserService  - all methods: GET, POST, DELETE, PUT are working

--- backend/src/main/java/com/maciejklonicki/org/backend/service/UserService.java
+++ backend/src/main/java/com/maciejklonicki/org/backend/service/UserService.java
@@ -1,8 +1,43 @@
 package com.maciejklonicki.org.backend.service;
 
+import com.maciejklonicki.org.backend.model.Users;
+import com.maciejklonicki.org.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    //getting all users
+    public List<Users> findAllUsers () {
+        return userRepository.findAll();
+    }
+
+    //getting single user
+    public Users findSingleUser (Integer id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    //adding new user
+    public void addNewUser (Users users) {
+        userRepository.save(users);
+    }
+
+    //delete user
+    public void deleteUser (Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    //update user
+    public void updateUser (Users users, Integer userID) {
+        userRepository.save(users);
+    }
+
 
 }

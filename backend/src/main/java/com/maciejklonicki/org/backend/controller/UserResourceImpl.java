f768113fe16cbde122d19205d3ba7f0ee368291f
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-10 11:38:36 +0200

    I managed to connect my backend with frontend - logging is working as expected

--- backend/src/main/java/com/maciejklonicki/org/backend/controller/UserResourceImpl.java
+++ backend/src/main/java/com/maciejklonicki/org/backend/controller/UserResourceImpl.java
@@ -14,14 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserResourceImpl {
 
     private static final Logger log = LoggerFactory.getLogger(UserResourceImpl.class);

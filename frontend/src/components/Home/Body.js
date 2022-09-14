67e103f7d3a3d548bcb6926d40fac8edf81ce534
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-14 10:07:01 +0200

    I made small changes with utils

--- frontend/src/components/Home/Body.js
+++ frontend/src/components/Home/Body.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 
 class Body extends Component {
 
+
   render(){
 
     const guestLinks = (
--- frontend/src/components/services/store.js
+++ frontend/src/components/services/store.js
@@ -4,4 +4,5 @@ import rootReducer from './rootReducer';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+
 export default store;

06bf8600818030bf8c8d23adcd4c2449fb3b822d
skay <maciek.klonicki@gmail.com>
2022-10-26 16:04:12 +0200

    Test are done

--- backend/src/test/java/com/maciejklonicki/org/backend/controller/UserControllerTest.java
+++ backend/src/test/java/com/maciejklonicki/org/backend/controller/UserControllerTest.java
@@ -1,26 +1,46 @@
 package com.maciejklonicki.org.backend.controller;
 
+import com.maciejklonicki.org.backend.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
+
+    @Mock private UserService userService;
+    @InjectMocks private UserController userController;
+
+    @BeforeEach
+    public void setUp() {
+        userController = new UserController(userService);
+    }
 
     @Test
     void findAllUsers() {
+        //when
+        userController.findAllUsers();
+        //then
+        verify(userService).findAllUsers();
     }
 
     @Test
     void getSingleUser() {
+        //when
+        userController.getSingleUser(15L);
+        //then
+        userService.findSingleUser(15L);
     }
-
-    @Test
-    void addNewUser() {
-    }
-
     @Test
     void deleteUser() {
-    }
-
-    @Test
-    void updateUser() {
+        //when
+        userController.deleteUser(5L);
+        //then
+        verify(userService).deleteUser(5L);
     }
 }
